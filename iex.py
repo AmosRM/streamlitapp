617f8766f86d896b0b120283db4dd7ed9eef53b5
AmosRM <88614465+AmosRM@users.noreply.github.com>
2022-06-29 12:43:51 +0800

    creating a new app

---
+++ iex.py
@@ -0,0 +1,58 @@
+import requests
+
+IEX_TOKEN = 'pk_a76555e5564549eb8277b0960f70135e'
+
+class IEXStock:
+
+    def __init__(self, token, symbol):
+        self.BASE_URL = 'https://cloud.iexapis.com/stable'
+        self.token = token
+        self.symbol = symbol
+
+    def get_logo(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/logo?token={IEX_TOKEN}"
+        r = requests.get(url)
+
+        return r.json()
+
+    def get_company_info(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/company?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
+    
+    def get_company_news(self, last=10):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/news/last/{last}?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
+
+    def get_stats(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/stats?token={self.token}"
+        r = requests.get(url)
+        
+        return r.json()
+
+    def get_fundamentals(self, period='quarterly', last=4):
+        url = f"{self.BASE_URL}/time-series/fundamentals/{self.symbol}/{period}?last={last}&token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
+
+    def get_dividends(self, range='5y'):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/dividends/{range}?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
+
+    def get_institutional_ownership(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/institutional-ownership?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
+
+    def get_insider_transactions(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/insider-transactions?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
---
+++ stocksapp.py
@@ -0,0 +1,70 @@
+from telnetlib import LOGOUT
+from urllib import response
+import pandas as pd
+import numpy as np
+from sympy import comp
+import streamlit as st
+import requests
+import config, iex
+
+IEX_TOKEN = 'pk_a76555e5564549eb8277b0960f70135e'
+
+symbol = st.sidebar.text_input("Symbol", value='MSFT')
+
+stock = iex.IEXStock(config.IEX_TOKEN, symbol)
+
+screen = st.sidebar.selectbox("View", ('Overview', 'Fundamentals', 'Technicals'))
+
+st.write(symbol)
+
+st.title(screen)
+
+if screen == 'Overview':
+    company = stock.get_company_info()
+    logo = stock.get_logo()
+
+    col1, col2 = st.columns([1, 4])
+
+    with col1:
+        st.image(logo['url'])
+
+    with col2:    
+        st.subheader(company['companyName'])
+        st.write(company['industry'])
+        st.subheader('Description')
+        st.write(company['description'])
+        st.subheader('CEO')
+        st.write(company['CEO'])
+
+def format_number(number):
+    return f"{number:,}"
+
+if screen == 'Fundamentals':
+    stats = stock.get_stats()
+
+    st.header('Ratios')
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader('P/E')
+        st.write(stats['peRatio'])
+        st.subheader('Marketcap')
+        st.write(stats['marketcap'])
+        st.subheader('Employees')
+        st.write(stats['employees'])
+        st.subheader('ttm EPS')
+        st.write(stats['ttmEPS'])
+        st.subheader('Beta')
+        st.write(stats['beta'])
+    with col2:
+        st.subheader('Revenue')
+        # st.write(format_number(stats['revenue']))
+        st.subheader('Cash')
+        # st.write(format_number(stats['totalCash']))
+        st.subheader('Debt')
+        #st.write(format_number(stats['currentDebt']))
+        st.subheader('200 Day Moving Average')
+        #st.write(stats['day200MovingAvg'])
+        st.subheader('50 Day Moving Average')
+        #st.write(stats['day50MovingAvg'])
