ada8e59c21b622b608fa5d03142567c3889375f7
AmosRM <88614465+AmosRM@users.noreply.github.com>
2022-09-19 10:43:59 +0800

    stockdash

--- StockApp/stockdashboard/app.py
+++ StockApp/stockdashboard/app.py
@@ -8,6 +8,17 @@ import config
 import psycopg2, psycopg2.extras
 import plotly.graph_objects as go
 
+# Twitter API Key -
+
+# auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+# auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
+# api = tweepy.API(auth)
+
+# DataBase connection with psycpg2 -
+
+# connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
+# cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
 
 option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'chart', 'pattern'), 3)
 
