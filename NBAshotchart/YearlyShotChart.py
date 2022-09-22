9e9ef07c29ef3f22f7e1430cbb2012ef79a6bcaf
AmosRM <88614465+AmosRM@users.noreply.github.com>
2022-09-22 10:42:33 +0800

    gif maker

--- NBAshotchart/YearlyShotChart.py
+++ NBAshotchart/YearlyShotChart.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+
 from shot_chart import draw_court, get_league_shots, shot_chart
 
 plt.rc('font', family='serif')
-choose_year = 10
+choose_year = 11
 
 # DATA
 df = pd.read_csv(f'shots_{choose_year}.csv')
@@ -22,14 +24,33 @@ shot_chart_options = {
     'gridsize': 50,
     'legend_args': {'columnspacing': 0.1},}
 
-title = f'NBA Leage-wide Shot Chart: {years}\nMinimum {threshold} Attempts'
+# title = f'NBA Leage-wide Shot Chart: {years}\nMinimum {threshold} Attempts'
 
 
 # PLOT
+# fig, ax = plt.subplots(figsize=(12,8), dpi=100)
+# shot_chart(df, ax, **shot_chart_options, title=title)
+# draw_court(ax=ax)
+# plt.xlim(-260, 260)
+# plt.ylim(-60, 300)
+# plt.axis('off')
+# plt.show()
 fig, ax = plt.subplots(figsize=(12,8), dpi=100)
-shot_chart(df, ax, **shot_chart_options, title=title)
-draw_court(ax=ax)
-plt.xlim(-260, 260)
-plt.ylim(-60, 300)
-plt.axis('off')
-plt.show()
+
+def plot(choose_year):
+    title = f'NBA Leage-wide Shot Chart: {choose_year}\nMinimum {threshold} Attempts'
+    ax.clear()
+    shot_chart(df, ax, **shot_chart_options, title=title)
+    draw_court(ax=ax)
+    plt.xlim(-260, 260)
+    plt.ylim(-60, 300)
+    plt.axis('off')
+    plt.show()
+
+anim = FuncAnimation(
+    fig,
+    plot,
+    frames=range(16,22,1)
+)
+
+anim.save('shot.gif', dpi=300, writer=PillowWriter(fps=5))
